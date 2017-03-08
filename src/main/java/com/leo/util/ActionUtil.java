3584530164be6ff91937ce9a63d5c4dbc85260dc
李宝宝 <798112618@qq.com>
2017-03-08 18:26:47 +0800

    Action工具类

---
+++ src/main/java/com/leo/util/ActionUtil.java
@@ -0,0 +1,23 @@
+package com.leo.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ActionUtil {
+	protected static Logger logger = LoggerFactory.getLogger("MainController");
+	public static String do1(String machineCode){
+		long start = System.currentTimeMillis();
+		MyRobot.click("D:\\HardWareId1.png",256,13);
+		MyRobot.selectAll(0);
+		MyRobot.del(1,0,0);
+		ClipboardUtil.setSysClipboardText(machineCode);
+		MyRobot.paste(0);
+		MyRobot.click("D:\\Generate.png",108,182);
+		MyRobot.click("D:\\copy.png");
+		MyRobot.move(0, 0);
+		String regCode =ClipboardUtil.getSysClipboardText();
+		long end = System.currentTimeMillis();
+		logger.info(regCode+"用时："+(end-start)/1000+"s");
+		return regCode;
+	}
+}
